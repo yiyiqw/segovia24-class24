440411a6057f9032649fdb2e2b98073612ef440a
yiyiq <yiyiqiu2004@gmail.com>
2024-03-14 10:24:10 +0100

    commit

--- practice.py
+++ practice.py
@@ -1,2 +1,11 @@
-import random
-import datetime
+# class code
+with open("secret.txt") as f:
+    text = f.read()
+text = text.split("\n")
+decooded = ""
+for line in text:
+    num_vow = 0
+    for v in "aeiou":
+        num_vow += line.count(v)
+    decoded = decoded + "abcdefghijklmnopqrstuvxyz"[num_vow]
+print(decoded)
